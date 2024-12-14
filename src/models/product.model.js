1941a742c8c06317f658e5e1290a41463f194c30
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-12-14 22:44:24 +0600

    add product model with schema definition and pagination support

---
+++ src/models/product.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true,
+        },
+        brand: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Brand",
+            required: true,
+        },
+        image: {
+            type: [String],
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        discount: {
+            type: Number,
+            default: 0,
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        rating: {
+            type: Number,
+            default: 0,
+        },
+        numReviews: {
+            type: Number,
+            default: 0,
+        },
+        isFeatured: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+productSchema.plugin(mongooseAggregatePaginate);
+
+const Product = mongoose.model("Product", productSchema);
