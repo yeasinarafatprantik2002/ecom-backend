4e6579821d8a9859c338c734224cf4d5347ff240
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-12-14 22:46:43 +0600

    add brand model with schema definition and pagination support

---
+++ src/models/brand.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const brandSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+    website: {
+        type: String,
+        default: "",
+    },
+    phone: {
+        type: String,
+        default: "",
+    },
+    email: {
+        type: String,
+        default: "",
+    },
+    address: {
+        type: String,
+        default: "",
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active",
+    },
+},{ timestamps: true });
+
+brandSchema.plugin(mongooseAggregatePaginate);
+
+const Brand = mongoose.model("Brand", brandSchema);
