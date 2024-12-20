6a6274e7970def90468e0b3b17c2236d7869081d
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-12-20 21:53:19 +0600

    refactor rating model to include product reference and enforce rating constraints

--- src/models/rating.model.js
+++ src/models/rating.model.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
-const ratingSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+const ratingSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        rating: {
+            type: Number,
+            enum: [0, 1, 2, 3, 4, 5],
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
     },
-    product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-    },
-    comment: {
-        type: String,
-        required: true,
-    },
-}, { timestamps: true });
+    { timestamps: true }
+);
 
 export const Rating = mongoose.model("Rating", ratingSchema);
